f258e4cc30fc739a0f2602077c7e0f94c482a82a
marco junior <marcojuniorengineer@gmail.com>
2022-02-09 17:49:50 -0300

    finalização do layoult

--- script.js
+++ script.js
@@ -4,16 +4,15 @@ let bot = document.querySelector("footer");
 let log = document.querySelector("section")
 let sidebar = document.querySelector("aside");
 let send = document.querySelector("send");
-let content = document.querySelector("body")
+let middle = document.querySelector(".middle")
 
 
-function functionWaiter(){
+function displayChat(){
     high.classList.remove("hidden");
     bot.classList.remove("hidden")
     log.classList.add("hidden");
 }
 function sendTo(){
     sidebar.classList.remove("hidden");
-    content.classList.add("dark");
-    high.classList.add("dark");
+    middle.classList.add("dark");
 }
